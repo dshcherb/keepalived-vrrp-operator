8d5c048c595ad80fca11290ffeca33cb5a7ea6ee
Dmitrii Shcherbakov <dmitrii.shcherbakov@canonical.com>
2020-03-31 00:40:36 +0300

    add support for relating a primary (e.g. haproxy)

--- src/charm.py
+++ src/charm.py
@@ -8,10 +8,13 @@ sys.path.append('lib') # noqa
 
 from ops.framework import (
     EventBase,
-    EventsBase,
-    CharmBase,
+    EventSource,
     StoredState,
 )
+
+from ops.model import ActiveStatus, MaintenanceStatus
+
+from ops.charm import CharmBase, CharmEvents
 
 from ops.main import main
 from pathlib import Path
@@ -27,8 +30,8 @@ class KeepalivedInitializedEvent(EventBase):
     pass
 
 
-class KeepalivedEvents(EventsBase):
-    pass
+class KeepalivedEvents(CharmEvents):
+    keepalived_initialized = EventSource(KeepalivedInitializedEvent)
 
 
 class KeepalivedCharm(CharmBase):
@@ -40,30 +43,45 @@ class KeepalivedCharm(CharmBase):
         super().__init__(*args)
 
         self.keepalived_conf_file = Path(f'/etc/keepalived/juju-{self.app.name}.cfg')
+        self.service_name = f'juju-{self.app.name}'
+        self.keepalived_service_file = Path(f'/etc/systemd/system/{self.service_name}.service')
 
         self.framework.observe(self.on.install, self.on_install)
+        self.framework.observe(self.on.upgrade_charm, self.on_upgrade_charm)
         self.peers = KeepalivedPeers(self, 'keepalived-peers')
         self.primary = VRRPParametersRequires(self, 'vrrp-parameters')
+        self.framework.observe(self.primary.on.primary_changed, self.on_primary_changed)
 
     def on_install(self, event):
         subprocess.check_call(['apt', 'update'])
         subprocess.check_call(['apt', 'install', '-yq', 'keepalived'])
         self.on.keepalived_initialized.emit()
+        self.unit.status = MaintenanceStatus('Waiting for a primary app unit to provide'
+                                             ' VRRP instance parameters.')
+
+    def on_upgrade_charm(self, event):
+        self.update_config()
 
     def on_primary_changed(self, event):
         self.update_config()
 
     def update_config(self):
-        ctxt = {
+        config_ctxt = {
             'is_initial': self.peers.initial_unit == self.model.unit.name,
             'vrrp_instances': self.primary.vrrp_instances
         }
         env = Environment(loader=FileSystemLoader('templates'))
-        template = env.get_template('keepalived.conf.j2')
-        rendered_content = template.render(ctxt)
-        self.haproxy_conf_file.write_text(rendered_content)
+        config_template = env.get_template('keepalived.conf.j2')
+        self.keepalived_conf_file.write_text(config_template.render(config_ctxt))
 
-        subprocess.check_call(['systemctl', 'restart', 'keepalived'])
+        service_ctxt = {
+            'keepalived_config': self.keepalived_conf_file
+        }
+        service_template = env.get_template('keepalived.service.j2')
+        self.keepalived_service_file.write_text(service_template.render(service_ctxt))
+        subprocess.check_call(['systemctl', 'daemon-reload'])
+        subprocess.check_call(['systemctl', 'restart', self.service_name])
+        self.unit.status = ActiveStatus()
 
 
 if __name__ == '__main__':
